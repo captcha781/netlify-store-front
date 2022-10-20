4ccb9930b13551c5a1ffd76a55e6e6d76fc2b263
captcha781 <bhuvanesh19112001@gmail.com>
2022-10-20 13:55:00 +0530

    redirects change

--- src/Pages/MainPages/Orders.tsx
+++ src/Pages/MainPages/Orders.tsx
@@ -9,38 +9,42 @@ import { OrderItem } from '../../types'
 
 const Orders = () => {
 
-  const orders:OrderItem[] = useAppState(state => state.common.orders)
+  const orders: OrderItem[] = useAppState(state => state.common.orders)
   const auth = useAppState(state => state.user.auth)
   const dispatch = useAppDispatch()
 
   document.title = "KeyStone | Orders"
 
   useEffect(() => {
-      axios.get("/user/orders")
+    axios.get("/user/orders")
       .then(response => {
         dispatch(setupOrders(response.data.orders))
         console.log(response);
-        
+
       })
       .catch(err => {
         message.error(err.message)
       })
-    
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   if (!auth) {
     return (
-        <div className="container wow fadeIn">
-            <Link to={"/signin"}><h2>Sign in to view Orders</h2></Link>
-        </div>
+      <div className="container wow fadeIn">
+        <Link to={"/signin"}><h2>Sign in to view Orders</h2></Link>
+      </div>
     )
-}
+  }
 
   localStorage.removeItem("entryurl")
   return (
     <div>
-      {orders.map(order => <OrderCard key={order._id} order={order}/>)}
+      {orders?.length > 0 ?
+        <>{orders?.map(order => <OrderCard key={order._id} order={order} />)}</>
+        :
+        <><h1>No Orders Found</h1></>
+      }
     </div>
   )
 }
